3967a08747401f7c5bd4505d60342aa3530a92be
Aarushi0010 <mahajanaarushi10@gmail.com>
2025-01-08 17:15:22 +0530

    firebase setup complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_app/screens/auth_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyDY16vbjVg24rhMayUE8tUBWPAFSdNA9w0",
+            appId: "1:593103971459:web:f45328df86ae90631367b6",
+            messagingSenderId: "593103971459",
+            projectId: "flutter-chat-app-a124f",
+            storageBucket: "flutter-chat-app-a124f.firebasestorage.app"));
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const App());
 }
 
